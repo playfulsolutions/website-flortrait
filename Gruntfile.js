09aa93478ce8f169bf7b8e7f1dcbda1242860945
Tobias Israel <tobias.israel@buschmais.com>
2018-08-06 12:28:57 +0200

    added required "Datenschutzerklärung"

--- Gruntfile.js
+++ Gruntfile.js
@@ -394,13 +394,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
-
     // Copies remaining files to places other tasks can use
     copy: {
       dist: {
@@ -499,7 +492,6 @@ module.exports = function (grunt) {
     'concat',
     'ngAnnotate',
     'copy:dist',
-    'cdnify',
     'cssmin',
     'uglify',
     'filerev',
