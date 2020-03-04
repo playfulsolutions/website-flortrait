932f8e26343444dc7d442c8be72cbd7c708189be
Tobias Israel <tobias.israel@buschmais.com>
2020-03-04 21:37:41 +0100

    update postal address

--- app/scripts/controllers/contact.controller.js
+++ app/scripts/controllers/contact.controller.js
@@ -12,8 +12,8 @@ angular.module('pfswebApp')
     var vm = this;
 
     vm.googleMapsUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBtakm4cEJbXhAde2tX4U0yKJTjMOG5Zp0';
-    vm.latitude = 51.055163;
-    vm.longitude = 13.724929;
+    vm.latitude = 51.046876;
+    vm.longitude = 13.774954;
     vm.center = JSON.stringify([vm.latitude, vm.longitude]);
 
   });
